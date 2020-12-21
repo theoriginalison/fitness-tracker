2410614c630bf8ff82ba35394e85da89ec22b6b4
Alison Lubar <aslubar@gmail.com>
2020-12-21 18:40:52 -0500

    completed html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,6 +5,13 @@ router.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/exercise.html"))
 })
 
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"))
+})
+
+router.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const htmlRoutes = require("./routes/html-routes");
-const apiRoutes = require("./routes/api-routes");
+// const apiRoutes = require("./routes/api-routes");
 
 const PORT = process.env.PORT || 3000;
 
@@ -18,7 +18,7 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
 
 app.use(htmlRoutes);
-app.use(apiRoutes);
+// app.use(apiRoutes);
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
