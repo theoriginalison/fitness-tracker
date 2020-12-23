710ad5ed05e70ac2b56ed093d6a75664001be28c
Alison Lubar <aslubar@gmail.com>
2020-12-22 19:09:13 -0500

    completed models directory

---
+++ models/workout.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                required: true,
+            },
+            name: {
+                type: String,
+                required: true,
+            },
+            duration: {
+                type: Number,
+                required: true,
+            },
+            weight: Number,
+            reps: Number,
+            sets: Number,
+            distance: Number
+
+        },
+
+    ]
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 app.use(htmlRoutes);
 // app.use(apiRoutes);
